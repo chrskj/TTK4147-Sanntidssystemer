7061fa66f390c8fe117676a3a5d7c98c7d1d14d6
chrskj <christofer.skjaeveland@gmail.com>
2019-09-10 09:13:56 +0200

    restructure main

--- ex1/task_b/main.c
+++ ex1/task_b/main.c
@@ -4,7 +4,17 @@
 
 int main(int argc, char *argv[]){
     if(argc == 2){
-        printf("Too many arguments supplied.\n");
+        switch (*argv[1]){
+            case 'a':
+                printf("This is a");
+                break;
+            case 'b':
+                printf("This is b");
+                break;
+            case 'c':
+                printf("This is c\n");
+                break;
+        }
     }
     else if(argc > 2){
         printf("Too many arguments supplied.\n");
